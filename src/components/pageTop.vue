9815440dffca85e2f08401d6201158767a6c1515
snow <1529836290@qq.com>
2022-10-02 16:50:31 +0800

    完成

--- src/components/pageTop.vue
+++ src/components/pageTop.vue
@@ -1,10 +1,17 @@
 <template>
-  <van-notice-bar left-icon="volume-o" text="网站图片皆来源于超神影业官方微博，不做任何商业行为和盈利，不损失版权方任何利益，仅限于收藏使用，个人欣赏，不注释，改编，不推广和宣传以及引流。" />
+  <van-notice-bar
+    left-icon="volume-o"
+    text="网站图片皆来源于超神影业官方微博，不做任何商业行为和盈利，不损失版权方任何利益，仅限于收藏使用，个人欣赏，不注释，改编，不推广和宣传以及引流。"
+  />
   <div class="top">
     <img class="logo" src="../assets/logo.png" />
     <img class="menuIcon" src="../assets/菜单.png" @click="showPopup" />
   </div>
-  <van-popup v-model:show="show" position="right" :style="{ width: '40%', height: '100%' }">
+  <van-popup
+    v-model:show="show"
+    position="right"
+    :style="{ width: '40%', height: '100%' }"
+  >
     <!-- <van-cell-group>
       <van-cell
         @click="jump(item.name)"
@@ -15,65 +22,68 @@
       />
     </van-cell-group> -->
     <van-sidebar v-model="act">
-      <van-sidebar-item @click="jump(item.name, index)" v-for="(item, index) in menuList" :key="item"
-        :title="item.text" />
-
+      <van-sidebar-item
+        @click="jump(item.name, index)"
+        v-for="(item, index) in menuList"
+        :key="item"
+        :title="item.text"
+      />
     </van-sidebar>
-
   </van-popup>
 </template>
 
 <script>
-import { ref, getCurrentInstance } from 'vue'
+import { ref, getCurrentInstance } from "vue";
 import { useRouter } from "vue-router";
 export default {
-  name: 'pageTop',
+  name: "pageTop",
   setup() {
-
-    let { proxy } = getCurrentInstance()
-    console.log(proxy.$router.currentRoute.value.name)
-    const menuList = [{ name: 'Home', text: '首页' }, { name: 'WallPaper', text: '精美壁纸' },
-    { name: 'QPics', text: 'Q版雄兵连' },
-    //  { name: 'xiongThree', text: '雄三在线' },
-    { name: 'activePics', text: '动态壁纸' },
-    { name: 'Us', text: '支持我们' }
-    ]
+    let { proxy } = getCurrentInstance();
+    console.log(proxy.$router.currentRoute.value.name);
+    const menuList = [
+      { name: "Home", text: "首页" },
+      { name: "WallPaper", text: "精美壁纸" },
+      { name: "QPics", text: "Q版雄兵连" },
+      { name: "xiongThree", text: "雄三在线" },
+      { name: "activePics", text: "动态壁纸" },
+      { name: "Us", text: "支持我们" },
+    ];
     const show = ref(false);
-    const act = ref(0)
+    const act = ref(0);
     const showPopup = () => {
       show.value = true;
-      act.value = sessionStorage.getItem('i')
-      console.log(act.value)
+      act.value = sessionStorage.getItem("i");
+      console.log(act.value);
     };
     let jump = function (name, i) {
-
-      console.log(typeof i)
-      sessionStorage.setItem('i', i)
+      console.log(typeof i);
+      sessionStorage.setItem("i", i);
 
       if (proxy.$router.currentRoute.value.name == name) {
         show.value = false;
       } else {
         show.value = false;
-        proxy.$router.push({ name: name })
-
+        proxy.$router.push({ name: name });
       }
-
-    }
+    };
     return {
-      show, showPopup, menuList, jump, act
-    }
-  }
-}
+      show,
+      showPopup,
+      menuList,
+      jump,
+      act,
+    };
+  },
+};
 </script>
 <style lang="less">
 :root {
   --sidebarW: 100%;
 }
 
-
 .van-sidebar {
   width: var(--sidebarW) !important;
-  background-color: #EDEDED;
+  background-color: #ededed;
   height: 100%;
 
   .van-sidebar-item--select:before {
@@ -91,7 +101,7 @@ export default {
     margin-bottom: 20px;
     height: 80px;
     line-height: 40px;
-    background-color: #EDEDED;
+    background-color: #ededed;
   }
 
   .van-sidebar-item--select,
@@ -129,6 +139,5 @@ export default {
     transform: translateY(-50%);
     margin-right: 15px;
   }
-
 }
 </style>
