ec32626635f98ad148b6378e64d648de394460fc
snow <1529836290@qq.com>
2022-10-03 21:09:19 +0800

    完成

--- src/views/PicDownload.vue
+++ src/views/PicDownload.vue
@@ -4,7 +4,7 @@
 
     <img class="title_img" src="../assets/wallPicBackGround.png">
     <img class="pic" ref="Vimg" style="position: absolute; z-index: -100; visibility: hidden;" :src="picAddress">
-    <canvas ref="canva" class="pic" id="myCanvas" style="background:transparent;">您的浏览器不支持Canvas!</canvas>
+    <canvas ref="canva" class="can" id="myCanvas" style="background:transparent;">您的浏览器不支持Canvas!</canvas>
     <a id="bottom-1" class="downLoadBtn" :href="picAddress + '?response-content-type=application/octet-stream'"
       download>
 
@@ -95,11 +95,18 @@ export default {
     transform: translateX(-50%);
   }
 
-  .pic {
+  .can {
     width: 90%;
     margin-top: 20px;
     margin-left: 5%;
     height: v-bind('actHeight.height');
+  }
+
+  .pic {
+    width: 90%;
+    margin-top: 20px;
+    margin-left: 5%;
+    height: auto;
   }
 }
 
