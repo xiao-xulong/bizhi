58f6d8f34349644d76889619c03fda7856f8c560
snow <1529836290@qq.com>
2022-10-03 19:57:10 +0800

    完成

--- src/views/PicDownload.vue
+++ src/views/PicDownload.vue
@@ -44,7 +44,7 @@ export default {
         let ctx = canvas.getContext("2d");
         canvas.height = Vimg.value.clientHeight
 
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, Vimg.value.clientHeight);
         let scale = window.devicePixelRatio;
         ctx.scale(scale, scale);
         // console.log(img.width + ":" + img.height);
